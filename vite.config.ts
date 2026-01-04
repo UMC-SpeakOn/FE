115de453c3bc060b223cd5102c19c0f33f035875
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-01-05 06:32:36 +0900

    🔨 Setting: pwa 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,54 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script-defer',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        'icons/favicon.ico',
+        'icons/apple-touch-icon.png',
+        'icons/masked-icon.svg',
+      ],
+      manifest: {
+        name: 'SpeakOn',
+        short_name: 'SpeakOn',
+        description: '9기 UMC 데모데이 프로젝트 SpeakOn',
+        lang: 'ko-KR',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
