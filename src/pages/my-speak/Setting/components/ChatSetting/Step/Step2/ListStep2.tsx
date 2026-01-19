df8103a0e91c68146918f7f54123ff556f7e1192
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-01-19 17:50:10 +0900

    🐛 Fix: title 반응형 오류 해결

--- src/pages/my-speak/Setting/components/ChatSetting/Step/Step2/ListStep2.tsx
+++ src/pages/my-speak/Setting/components/ChatSetting/Step/Step2/ListStep2.tsx
@@ -16,7 +16,7 @@ const ListStep2 = ({ selectedId, onSelect }: ListStep2Props) => {
         description="마지막 질문은 항상 정리 질문으로 마무리됩니다"
       />
 
-      <div className="w-full flex gap-[0.8rem] items-center">
+      <div className="w-full flex gap-[0.8rem] items-center max-[375px]:flex-col max-[375px]:items-stretch">
         {goalData.map((item) => (
           <ItemStep2
             key={item.id}
--- src/pages/my-speak/Setting/components/ChatSetting/Title/Title.tsx
+++ src/pages/my-speak/Setting/components/ChatSetting/Title/Title.tsx
@@ -5,7 +5,7 @@ type TitleProps = {
 
 const Title = ({ title, description }: TitleProps) => {
   return (
-    <div className="w-full flex items-center gap-[0.7rem] pr-[1.462rem]">
+    <div className="w-full flex items-center gap-[0.7rem] pr-[1.462rem] min-h-[3.2rem]">
       <p className="whitespace-nowrap font-semibold text-[1.6rem] text-black leading-none">
         {title}
       </p>
