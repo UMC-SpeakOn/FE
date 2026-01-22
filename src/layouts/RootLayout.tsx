70f3e662b366b6185a8d1a4bf8ec4aacaa014b93
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-01-22 14:36:48 +0900

    ♻️ Refactor: Header와 Navbar 분리

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,20 +2,28 @@ import { Outlet, useLocation } from 'react-router-dom';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
+import Navbar from '@/components/Navbar/Navbar';
 
 const RootLayout = () => {
-  const location = useLocation();
-  const hideHeader = location.pathname === '/my-speak/interview';
+  const { pathname } = useLocation();
+
+  const HIDE_NAV_PATHS = ['/my-speak/result', '/my-speak/interview'];
+
+  const hideNavbar = HIDE_NAV_PATHS.some((path) => pathname.startsWith(path));
 
   return (
     <div className="pageContainer">
       <div className="scrollArea scroll">
         <div className="screenSection">
-          {!hideHeader && <Header />}
+          {/* {!hideHeader && <Header />} */}
+          <Header />
+          {!hideNavbar && <Navbar />}
+
           <main className="mainSection">
             <Outlet />
           </main>
         </div>
+
         <Footer />
       </div>
     </div>
--- src/utils/header/getHeaderLabel.ts
+++
@@ -1,5 +0,0 @@
-import { HEADER_ITEMS } from '@/mocks/headerData';
-
-export const getHeaderLabel = (pathname: string) => {
-  return HEADER_ITEMS.find((item) => item.match(pathname))?.label ?? 'My Role';
-};
