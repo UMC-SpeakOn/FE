cf3810368818b911e31221eb6aad7cfc94ff1763
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-01-23 19:01:31 +0900

    🐛 Fix: Title 우측 여백 삭제

--- src/pages/my-report/detail/components/common/ReportSection/Title.tsx
+++ src/pages/my-report/detail/components/common/ReportSection/Title.tsx
@@ -5,7 +5,7 @@ type TitleProps = {
 
 const Title = ({ title, description }: TitleProps) => {
   return (
-    <div className="w-full flex items-center gap-[0.7rem] pr-[1.7rem] min-h-[2.3rem]">
+    <div className="w-full flex items-center gap-[0.7rem] min-h-[2.3rem]">
       <p className="whitespace-nowrap font-bold text-[1.9rem] text-black leading-none">
         {title}
       </p>
