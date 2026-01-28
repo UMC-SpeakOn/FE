6a22425a543a4d24288c7d8667737e546b3e3850
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-01-28 15:37:04 +0900

    🎨 Design: 롤 추가 모달 UI 구현 완료

---
+++ src/pages/my-role/components/Modal/AddModal.tsx
@@ -0,0 +1,46 @@
+import Check from '@/assets/images/icons/check.svg';
+import Close from '@/assets/images/icons/close.svg';
+
+interface AddModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const AddModal = ({ open, onClose }: AddModalProps) => {
+  if (!open) return null;
+
+  return (
+    <div
+      onClick={onClose}
+      className="fixed inset-y-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] z-50 flex items-center justify-center bg-black/50 shadow-(--shadow-modal)"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        className="relative w-[29.4rem] h-[16.3rem] rounded-2xl bg-white flex justify-center pt-[2.8rem]"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img
+          src={Close}
+          alt="close"
+          onClick={onClose}
+          className="w-[1.9rem] cursor-pointer absolute right-[1.3rem] top-4"
+        />
+
+        <div className="flex flex-col gap-[1.2rem] items-center">
+          <img src={Check} alt="check" className="w-[3.9rem]" />
+          <p className="text-[1.5rem] font-bold text-black text-center leading-none">
+            등록 완료
+          </p>
+          <p className="text-[1.2rem] font-semibold text-black text-center leading-[1.3]">
+            메뉴 → My Speak으로
+            <br />
+            이동해서 학습을 시작하세요!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddModal;
