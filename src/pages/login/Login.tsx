e0d7dcac800ccf279fa2ead63cb0c7702481c958
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-02-05 18:37:19 +0900

    🔥 Delete: 필요없는 UI 삭제

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -7,7 +7,6 @@ import { getRedirectUri } from '@/pages/login/utils/kakao/getRedirectUri';
 
 import ListButton from './components/Button/ListButton';
 import ListDecoration from './components/Decoration/ListDecoration';
-import Navbar from './components/Navbar/Navbar';
 import { useKakaoLogin } from './hooks/useKakaoLogin';
 
 const Login = () => {
@@ -43,8 +42,6 @@ const Login = () => {
 
   return (
     <div className="pageContainer h-screen px-[1.6rem] relative flex flex-col items-center">
-      <Navbar />
-
       <div className="relative z-10 mt-[30%] flex flex-col gap-[6.827rem] text-center text-white font-bold leading-none">
         <p className="font-unbounded text-[3.866rem]">SpeakOn</p>
         <p className="text-[1.5rem]">
